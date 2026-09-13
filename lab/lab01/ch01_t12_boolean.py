113d3e2178b38485828bf41db722e8b33d381f6a
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-13 02:38:46 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -1,5 +1,7 @@
 # Hi! I'm Maria and I live in script.py.
 # I'm an expert Python coder.
 # I'm 21 years old and I plan to program cool stuff forever.
+a=True
+b=fail
 age_is_12 = b
 name_is_maria = a
