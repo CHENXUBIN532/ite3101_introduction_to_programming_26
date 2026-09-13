217295464a85915ffcb9a60193af3f04d01ca07f
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-13 02:13:46 +0000

    [skip actions] lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -2,4 +2,5 @@ cucumebers=100
 num_people=6
 whole_cucumbers_per_person=cucumebers/num_people
 print("whole_cucumbers_per_perosn")
-float_cucumbers_per_person=cucumebers/
+float_cucumbers_per_person=cucumebers/num_people
+print("float_cucumbers_per)
