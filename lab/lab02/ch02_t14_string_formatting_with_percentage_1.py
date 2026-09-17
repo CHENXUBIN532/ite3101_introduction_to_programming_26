a9f5c08deba17fb05dcd0fc469328da6e3d4acf2
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-17 08:44:41 +0000

    [skip actions] lab/lab02/ch02_t15_string_formatting_with_percentage_2.py

--- lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
+++ lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
@@ -2,5 +2,5 @@ string_1 = "Camelot"
 string_2 = "place"
 
 print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
-#the string1 will insert the first %
-the string2 will insert the second %
+# the string1 will insert the first %
+# the string2 will insert the second %
