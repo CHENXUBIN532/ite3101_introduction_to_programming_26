b40ac6f6e7844045eab44a63ef16c863bc4ffa92
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-17 08:54:54 +0000

    [skip actions] lab/lab02/ch02_t15_string_formatting_with_percentage_2.py

--- lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
+++ lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
@@ -1,9 +1,8 @@
-name = "xubin"("What is your name? ")
-quest = "working"("What is your quest? ")
-color = "green"("What is your favorite color? ")
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+color = input("What is your favorite color? ")
 
 # Uncomment the below 2 line of code!
 # print("Ah, so your name is ___, your quest is ___, "
 # "and your favorite color is ___." ___ (name, quest, color))
-print("Ah, so your name is %s,your quest is %s,and your favorite color is %s." %
-      ("xubin""working""green"))
+print("Ah, so your name is %s,your quest is %s,and your favorite color is %s"%("xubin""working""green"))
