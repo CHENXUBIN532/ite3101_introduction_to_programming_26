d7b27b31364aae2d8c3640e2c24f2453a4b6c00c
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-24 07:25:13 +0000

    lab/lab03/ch03_t03_extracting_Info.py

--- lab/lab03/ch03_t03_extracting_Info.py
+++ lab/lab03/ch03_t03_extracting_Info.py
@@ -2,9 +2,9 @@ from datetime import datetime
 
 now = datetime.now()
 print(now)
-current_year=now.year
+current_year = now.year
 print(current_year)
-current_month=now.month
+current_month = now.month
 print(current_month)
-current_day=now.day
-print()
+current_day = now.day
+print(current_day)
