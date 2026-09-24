13e0ac780e87949a85c4df3a44b9f0dfe05479c8
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-24 14:00:41 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -4,9 +4,9 @@ def grade_converter(grade: int) -> str:
         return "A"
     elif grade_converter > 70:
         return "B"
-    elif grade_converter=70:
+    elif grade_converter = 70:
         return "C"
-    elif :
+    elif grade_converter > 61:
         return "D"
     else:
         return "F"
