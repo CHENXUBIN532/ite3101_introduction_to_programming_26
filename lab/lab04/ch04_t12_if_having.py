d27ab5bf2c15127c50928d7c6ac282670606fbf1
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-24 08:42:25 +0000

    [skip actions] lab/lab04/ch04_t12_if_having.py

--- lab/lab04/ch04_t12_if_having.py
+++ lab/lab04/ch04_t12_if_having.py
@@ -1,13 +1,12 @@
 def using_control_once() -> str:
-    if 5>2
-        return "Success #1"
+    if 5 > 2
+    return "Success #1"
 
 
 def using_control_again() -> str:
-    if None:
-        return "Success #2"
+    if 3 > 2
+    return "Success #2"
 
 
 print(using_control_once())
 print(using_control_again())
-
