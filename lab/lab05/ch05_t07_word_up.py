f9cda671970c0f88bbed42771a50958e3dbedfc5
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-25 01:35:50 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -3,7 +3,7 @@ pyg = 'ay'
 original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha() :
-    word = the.string[0].lower()
+    word = the.string.lower()
     first = word[0]
 
     print(original)
