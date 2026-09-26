423da90c65ca5448bd91b2a8299b721364c75bf2
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-26 09:33:05 +0000

    lab/lab06/ch06_t06_prac_makes_perfect.py

--- lab/lab06/ch06_t06_prac_makes_perfect.py
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -1,9 +1,10 @@
-def cube(number) :
+def cube(number):
     return number ** 3
 
+
 def by_three(number):
-    if number % 3 ==0 :
-       number=cube(number)
-       return cube(number)
+    if number % 3 == 0:
+
+        return cube(number)
     else:
         return False
