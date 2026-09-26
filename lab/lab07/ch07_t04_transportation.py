9d59b873ac1419d5320ae29327fe17914d215f7a
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-26 13:17:08 +0000

    lab/lab07/ch07_t04_transportation.py

--- lab/lab07/ch07_t04_transportation.py
+++ lab/lab07/ch07_t04_transportation.py
@@ -16,6 +16,7 @@ def plane_ride_cost(city: str) -> int:
 def rental_car_cost(days):
     if days >= 7:
         return 40*days-50
-    elif days >=3 and days < 7:
+    elif days >= 3 and days < 7:
         return 40*days-20
-    else return
+    else:
+        return days*40
