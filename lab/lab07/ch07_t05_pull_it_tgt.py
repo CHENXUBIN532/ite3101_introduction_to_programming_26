b2b7f1b44813a0101573f7c43b3b0df126c3d27c
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-26 13:28:25 +0000

    lab/lab07/ch07_t05_pull_it_tgt.py

--- lab/lab07/ch07_t05_pull_it_tgt.py
+++ lab/lab07/ch07_t05_pull_it_tgt.py
@@ -21,7 +21,7 @@ def rental_car_cost(days: int) -> int:
         cost -= 20
     return cost
 
-def trip_cost(days,city):
+def trip_cost(city,days):
     return rental_car_cost(days)+plane_ride_cost(city)+hotel_cost(days-1)
 
 
