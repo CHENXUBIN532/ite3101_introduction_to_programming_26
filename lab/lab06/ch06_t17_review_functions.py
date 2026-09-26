24dffa75ebb98ddff7e7d0ecb88de61af6f75faf
CHENXUBIN532 <260155731@stu.vtc.edu.hk>
2026-09-26 11:26:38 +0000

    [skip actions] lab/lab06/ch06_t17_review_functions.py

--- lab/lab06/ch06_t17_review_functions.py
+++ lab/lab06/ch06_t17_review_functions.py
@@ -1,7 +1,7 @@
 def shut_down(s:str) ：
     if s =="yes":
        return "Shutting down"
-    elif s==no:
+    elif s=="no":
         return shut_down("shutdown aborted")
     else:
         return shut_down("sorry")
